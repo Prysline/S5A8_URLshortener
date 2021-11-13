cf3eb728634a1ea09f51d3c313e3716ae7fcdec4
Prysline <mayhu1993@gmail.com>
2021-11-13 15:03:40 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = 3000
+
+mongoose.connect('mongodb://localhost/shorting-url')
+const db = mongoose.connection
+
+db.on('error', ()=> {
+  console.log('mongodb error!')
+})
+db.once('open', ()=> {
+  console.log('mongodb connected!')
+})
 
 app.get('/', (req, res) => {
   console.log(`Connected index.`)
---
+++ models/url.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const urlSchema = new Schema({
+  originalUrl: {
+    type: String,
+    require: true
+  },
+  shortingUrl: String
+})
+module.exports = mongoose.model('Url', urlSchema)
