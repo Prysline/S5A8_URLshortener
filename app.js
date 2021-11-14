107d9da376b31ea12abac1c653f057074a82888f
Prysline <mayhu1993@gmail.com>
2021-11-14 21:36:25 +0800

    feat: url shortener

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const routes = require('./routes')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 
 // Sever: listen
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,44 @@
 const express = require('express')
+const Url = require('../../models/url')
 const router = express.Router()
+const US = require('../../scripts/URLshortener')
 
 router.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', { logoURL: '#' })
+})
+
+router.post('/', (req, res) => {
+  const originalUrl = req.body.originalUrl
+  let shortingUrl = ''
+  Url.find()
+    .lean()
+    .then(urls => {
+      const UrlRepeat = US.checkUrlRepeat(urls, originalUrl)
+      if (UrlRepeat[0]) {
+        shortingUrl = UrlRepeat[1]
+      } else {
+        console.log('no existing');
+        shortingUrl = US.getNoRepeatShortingUrl(urls, 5)
+        Url.create({ originalUrl, shortingUrl })
+      }
+      res.render('success', { logoURL: '/', shortingUrl })
+    })
+    .catch(error => console.log(error))
+})
+
+// 轉址
+router.get('/:shortingUrl', (req, res) => {
+  const shortingUrl = req.params.shortingUrl
+  console.log(req.params);
+  let targetUrl = ''
+  Url.find()
+    .lean()
+    .then(urls => {
+      const target = urls.find(item => item.shortingUrl === shortingUrl)
+      targetUrl = target.originalUrl
+      res.redirect(targetUrl)
+    })
+    .catch(error => console.log(error))
 })
 
 module.exports = router
